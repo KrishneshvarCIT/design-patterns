959a3000ddf992c576cf201d03f8d01707b341d0
Krishneshvar J (CIT) <121788987+KrishneshvarCIT@users.noreply.github.com>
2024-07-24 00:07:40 +0530

    Update projector.java

--- Facade/projector.java
+++ Facade/projector.java
@@ -1,18 +1,12 @@
 
 package Facade;
-// Facade - class for Projector
 
 public class projector {
     public void on() {
         System.out.println("Projector is on");
     }
 
-    public void wideScreenMode() {
-        System.out.println("Projector is in widescreen mode");
-    }
-
     public void off() {
         System.out.println("Projector is off");
     }
 }
-
