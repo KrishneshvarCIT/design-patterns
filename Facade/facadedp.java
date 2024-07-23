42f92bece0fb4e359cd064eba0ff7aead4374e14
Krishneshvar J (CIT) <121788987+KrishneshvarCIT@users.noreply.github.com>
2024-07-24 00:06:33 +0530

    Update facadedp.java

--- Facade/facadedp.java
+++ Facade/facadedp.java
@@ -3,16 +3,14 @@ package Facade;
 
 public class facadedp {
     public static void main(String[] args) {
-        // Create instances of subsystem components
+
         dvdplayer dvdPlayer = new dvdplayer();
         amplifier amplifier = new amplifier();
         projector projector = new projector();
         screen screen = new screen();
 
-        // Create the facade
         hometheater homeTheater = new hometheater(dvdPlayer, amplifier, projector, screen);
 
-        // Use the facade to control the subsystem
         homeTheater.watchMovie("Avengers: The Endgame");
         homeTheater.endMovie();
     }
